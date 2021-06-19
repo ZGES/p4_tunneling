df364c65e9765a0830da2a7289353b6f2343098e
Piotr Pasternak <ppasternak@student.agh.edu.pl>
2021-06-19 13:05:38 -0400

    Now it PING

--- tunneling.p4app/foreigntopo.py
+++ tunneling.p4app/foreigntopo.py
@@ -16,9 +16,3 @@ class CustomAppTopo(AppTopo):
         Host3 = self.addHost( 'h3', ip='30.0.0.1/24', mac='00:00:00:00:00:03' )
         Switch1 = self.addSwitch('s1')
         Switch2 = self.addSwitch('s2')
-
-        # Add links
-        self.addLink( Host1, Switch2 )
-        self.addLink( Host2, Switch1 )
-        self.addLink( Host3, Switch2 )
-        self.addLink( Switch1, Switch2 )
